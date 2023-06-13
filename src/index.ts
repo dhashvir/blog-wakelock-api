2f6f9d35a59d1591893308da674d882a70eab013
Dhashvir Lalla <dhashvir@gmail.com>
2023-06-13 20:15:48 +1000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+const requestButton = document.querySelector("#request") as HTMLButtonElement;
+const releaseButton = document.querySelector("#release") as HTMLButtonElement;
+const info = document.querySelector("#info") as HTMLParagraphElement;
+
+let wakeLock: WakeLockSentinel | null; // Global Wake Lock to keep track of.
+
+function isWakeLockSupported() {
+  return "wakeLock" in navigator;
+}
+
+async function requestFn() {
+  await releaseFn();
+  return (wakeLock = await requestWakeLock());
+}
+
+async function releaseFn() {
+  const released = await wakeLock?.release();
+  wakeLock = null;
+  return released;
+}
+
+async function requestWakeLock() {
+  let wakeLock: WakeLockSentinel | null = null;
+  try {
+    wakeLock = await navigator.wakeLock.request("screen");
+    // Listen if screen waked has been released.
+    wakeLock.addEventListener("release", () => {
+      console.log(`Wake Lock released. Released: ${wakeLock?.released}`);
+      info.textContent = `Wake Lock released. Released: ${wakeLock?.released}`;
+    });
+
+    info.textContent = `Wake Lock is active. Released: ${wakeLock?.released}`;
+    console.log(`Wake Lock is active. Released: ${wakeLock?.released}`);
+  } catch (err) {
+    // The Wake Lock request has failed - usually system related, such as battery.
+  }
+  return wakeLock;
+}
+
+if (isWakeLockSupported()) {
+  requestButton.addEventListener("click", requestFn);
+  releaseButton.addEventListener("click", releaseFn);
+  info.textContent = "Wake Lock Supported.";
+} else {
+  requestButton.hidden = true;
+  releaseButton.hidden = true;
+  info.textContent = "Wake Lock Not Supported.";
+}
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "visible") {
+    // Do some state checks to determine if you want to reactivate wake lock
+    if (wakeLock?.released) {
+      requestFn();
+    }
+  }
+});
